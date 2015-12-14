cd8ccd85b1f58809483f41dfd530164c484be480
liqian <liqian@bilibili.com>
2015-12-14 21:08:10 +0800

    提交基础的测试结构

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');                            // 多个文件合并为一个；
+var minifyCss = require('gulp-minify-css');                     // 压缩CSS为一行；
+var rename = require('gulp-rename');                            // 文件名替换
+
+var baseUrl = './src/';
+gulp.task('default', function() {                               // 创建一个task
+  return gulp.src([baseUrl+'base.css',baseUrl+'default.css',baseUrl+'layout.css',baseUrl+'font.css'])
+    .pipe(concat('build.css'))                                 // 合并后的文件名
+    .pipe(gulp.dest('./dist'))                                 // 输出合并后的文件
+    .pipe(rename({suffix: '.min'}))                            // 加.min的后缀
+    .pipe(minifyCss())                                         // 压缩处理成一行
+    .pipe(gulp.dest('./dist'))                                 // 输出压缩后的文件
+});
+
